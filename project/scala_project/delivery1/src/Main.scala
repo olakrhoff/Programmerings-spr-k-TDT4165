0fbb93fd4b3bd164e8e891f724b7adf5b4b2dc34
olakristofferhoff <ola.kr@hotmail.no>
2022-11-01 17:25:56 +0100

    Feat: Task 2 also done, delivery 1 complete AKA project part 1

--- project/scala_project/delivery1/src/Main.scala
+++ project/scala_project/delivery1/src/Main.scala
@@ -1,5 +1,12 @@
-object Task1 extends App 
+import java.util.concurrent.atomic.AtomicInteger
+import scala.concurrent.duration._
+import scala.concurrent.Await
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Task1and2 extends App 
 {
+  //Task 1 b
   val sumArray = (array: Array[Int]) =>
   {
     var sum: Int = 0
@@ -9,6 +16,7 @@ object Task1 extends App
     sum
   }
 
+  //Task 1 c
   def sumArrayRecursive(array: Array[Int]): Int =
   {
     if (array.length == 0)
@@ -21,6 +29,7 @@ object Task1 extends App
     }
   }
 
+  //Task 1 d
   //F_0 = 0
   //F_1 = 1
   //F_N = F_(N-1) + F_(N-2)
@@ -37,23 +46,117 @@ object Task1 extends App
   }
 
   println("Starting task 1")
-  
+
+  //Task 1 a
   var numbers: Array[Int] = new Array(50)
   for(i <- 0 to 49)
     numbers(i) = i + 1
-
-  println(sumArray(numbers))
-  println(sumArrayRecursive(numbers))
-  
-  for (n <- 0 to 10)
-    println(fibonacciRecursive(n))
 
   for(i <- 0 to 49) 
   { 
     print(numbers(i))
     print(", ")
   }
+
+  //Task 1 b
+  println(sumArray(numbers))
+  //Task 1 c
+  println(sumArrayRecursive(numbers))
+  
+  for (n <- 0 to 10)
+    println(fibonacciRecursive(n))
+
   println("")
   println("Task 1 is done")
+  println("--------------------------------")
+  println("Starting task 2") 
+
+
+  //Task 2 a
+  def threadFunction(func: () => Unit): Thread =
+  {
+    new Thread
+    {
+      override def run()
+      {
+        func()
+      }
+    }
+  }
+  
+  var thread1 = threadFunction(() => println("Function called"))
+
+  thread1.start()
+  thread1.join()
+
+  //Task 2 b
+  private var counter: Int = 0
+  def increaseCounter(): Unit = 
+  {
+    counter += 1
+  }
+
+  def printCounter() =
+  {
+    print("Counter is equal to: ")
+    println(counter)
+  }
+  
+  threadFunction(increaseCounter).start
+  threadFunction(increaseCounter).start
+  var t1 = threadFunction(printCounter)
+  t1.start
+  t1.join
+
+  //Task 2 c
+  private var atomicCounter: AtomicInteger = new AtomicInteger
+  def safeIncreaseCounter(): Integer =
+  {
+    atomicCounter.incrementAndGet
+  }
+
+  def printAtomicCounter(): Unit =
+  {
+    print("Atomic counter: ")
+    println(atomicCounter.get)
+  }
+
+  threadFunction(safeIncreaseCounter).start
+  threadFunction(safeIncreaseCounter).start
+  var t2 = threadFunction(printAtomicCounter)
+  t2.start
+  t2.join
+
+  //Task 2 d
+  object ResourceA
+  {
+    lazy val someValue = 123
+    lazy val someDependetValue = ResourceB.someDependetValue
+  }
+  
+  object ResourceB {
+    lazy val someDependetValue = ResourceA.someValue
+  }
+  
+  object DeadLock
+  {
+    def deadlockFunction =
+    {
+      val result = Future.sequence(Seq(
+        Future
+        {
+          ResourceA.someDependetValue
+        },
+        Future
+        {
+          ResourceB.someDependetValue
+        }
+      ))
+      Await.result(result, 5.second)
+    }
+  }
+  DeadLock.deadlockFunction
+
+  println("Task 2 is done")
 }
 
